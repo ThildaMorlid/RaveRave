d316bcbf3f809076635ee93202675e1dbc07b866
ThildaMorlid <thildaberglindmorlid@gmail.com>
2024-06-03 16:41:22 +0200

    Fixade lite med smileykomponenten, tog bort en headermapp

--- frontend/src/components/SmileyContainer.tsx
+++ frontend/src/components/SmileyContainer.tsx
@@ -6,6 +6,9 @@ const SmileyContainer: React.FC = () => {
   const smileysRef = useRef<HTMLImageElement[]>([]);
 
   useEffect(() => {
+    // Kopiera den aktuella referensen till en lokal variabel för att undvika ändringar
+    const currentSmileys = smileysRef.current;
+
     // Funktioner för att hantera rotation
     const handleMouseEnter = (smiley: HTMLImageElement) => {
       gsap.to(smiley, { rotation: 360, duration: 2, ease: "none", repeat: -1 });
@@ -15,20 +18,22 @@ const SmileyContainer: React.FC = () => {
       gsap.to(smiley, { rotation: 0, duration: 2, ease: "none", overwrite: true });
     };
 
-    smileysRef.current.forEach((smiley, index) => {
+    // Lägg till eventlyssnare
+    currentSmileys.forEach((smiley) => {
       smiley.addEventListener('mouseenter', () => handleMouseEnter(smiley));
       smiley.addEventListener('mouseleave', () => handleMouseLeave(smiley));
     });
 
+    // Cleanup-funktion för att ta bort eventlyssnare
     return () => {
-      smileysRef.current.forEach(smiley => {
-        if (smiley) { 
+      currentSmileys.forEach(smiley => {
+        if (smiley) {
           smiley.removeEventListener('mouseenter', () => handleMouseEnter(smiley));
           smiley.removeEventListener('mouseleave', () => handleMouseLeave(smiley));
         }
       });
     };
-  }, []);
+  }, []); // Notera att beroendelistan är tom, vilket betyder att detta bara körs en gång vid mount
 
   const positions = Array.from({ length: 50 }, (_, index) => ({
     top: `${Math.random() * 120}%`,
@@ -41,7 +46,7 @@ const SmileyContainer: React.FC = () => {
       position: 'relative',
       width: '150vw',
       height: '150vh',
-      overflow: 'viseble',
+      overflow: 'visible',
       zIndex: 0
     }}>
       {positions.map((pos, index) => (
